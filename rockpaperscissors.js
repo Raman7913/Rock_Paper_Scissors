ed1e0563848a18e4a702f34c05125d2cd652e536
Raman7913 <raman7913523@gmail.com>
2024-02-20 13:52:30 +0545

    added a new function to play rounds
    first thought of doing it with && operator but then realized
    that i can nest the if statement

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -10,4 +10,42 @@ function getComputerChoice(){
         return "scissor";
     }
 }
-console.log(getComputerChoice());
+let userSelection=prompt("enter rock paper or scissor:");
+let computerSelection=getComputerChoice();
+playRound(userSelection,computerSelection);
+function playRound(userSelection,computerSelection){
+    console.log("user selected: " + userSelection + "\ncomputer selected: " + computerSelection);
+    if(userSelection=="rock"){
+        if(computerSelection=="rock"){
+            console.log("draw");
+        }
+        else if(computerSelection=="paper"){
+            console.log("computer wins");
+        }
+        else if(computerSelection=="scissor"){
+            console.log("user wins");
+        }
+    }
+    else if(userSelection=="paper"){
+        if(computerSelection=="rock"){
+            console.log("user wins");
+        }
+        else if(computerSelection=="paper"){
+            console.log("draw");
+        }
+        else if(computerSelection=="scissor"){
+            console.log("computer wins");
+        }
+    }
+    else if(userSelection=="scissor"){
+        if(computerSelection=="rock"){
+            console.log("computer wins");
+        }
+        else if(computerSelection=="scissor"){
+            console.log("draw");
+        }
+        else if(computerSelection=="paper"){
+            console.log("user wins");
+        }
+    }
+}
